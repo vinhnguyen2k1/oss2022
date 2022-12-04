0654862c75805d91f3cd4948b84bd87a4463e915
vinhnguyen2k1 <thanhvinh951@gmail.com>
2022-12-04 19:56:54 +0700

    Viết hàm tính số Fibonacci

---
+++ Header.h
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+#include<iostream>
+using namespace std;
+
+int Fibonacci(int n);
---
+++ Main.cpp
@@ -0,0 +1,12 @@
+﻿#include "Header.h"
+
+int main() {
+
+    int n;
+    cout << "Nhap n: ";
+    cin >> n;
+    cout << "So Fibonacci thu " << n << " la: " << Fibonacci(n) << endl;
+ 
+    system("pause");
+    return 0;
+}
